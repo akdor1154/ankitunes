9a0137b3398ed108c84a70f99d527768d616bda7
Jarrad Whitaker <akdor1154@gmail.com>
2021-06-06 11:28:40 +1000

    fix

--- tests/ui/conftest.py
+++ tests/ui/conftest.py
@@ -160,16 +160,18 @@ def with_wm():
 	if wm.returncode != 0:
 		raise subprocess.CalledProcessError(wm.returncode, wm.args)
 
-def screenshot():
+def screenshot(winId: int):
 	import subprocess
 
 	os.makedirs('screenshots', exist_ok=True)
+	import datetime
+	import itertools
+	file = os.path.join('screenshots', f'{datetime.datetime.now()}.png')
 
 	subprocess.run(
-		'scrot',
-		shell=True,
-		check=True,
-		cwd='screenshots'
+		['import', '-window', f'0x{winId:X}', file],
+		#shell=True,
+		check=True
 	)
 
 
@@ -202,7 +204,9 @@ def anki_running(qtbot: QtBot, ankiaddon_cmd: Optional[str], install_ankitunes: 
 				qtbot.addWidget(aqt.mw)
 				yield app
 			finally:
-				screenshot()
+				#winId = int(aqt.mw.winId())
+
+				#screenshot(winId)
 				# clean up what was spoiled
 				app.closeAllWindows()
 
